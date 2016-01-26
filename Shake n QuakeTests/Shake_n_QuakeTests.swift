e1386625729f446f49ab44d7254e1781a5d82de1
kitzmiller09 <kitzmiller09@hotmail.com>
2016-01-26 10:40:52 -0500

    Initial Commit

---
+++ Shake n QuakeTests/Shake_n_QuakeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shake_n_QuakeTests.swift
+//  Shake n QuakeTests
+//
+//  Created by Kitzmiller, Andrew L (Marketing Department) on 1/26/16.
+//  Copyright © 2016 Kitzmiller, Andrew L. All rights reserved.
+//
+
+import XCTest
+@testable import Shake_n_Quake
+
+class Shake_n_QuakeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
